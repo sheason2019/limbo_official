36b62ed051a2f81941c7c5d840fd6d959ab86ee2
sheason <811627877@qq.com>
2024-11-09 01:28:51 +0800

    初始化文档站

---
+++ blog/2024-11-09-hello-world.md
@@ -0,0 +1,8 @@
+---
+slug: hello-world
+title: Hello World
+authors: sheason
+tags: []
+---
+
+Hello world
---
+++ docusaurus.config.ts
@@ -0,0 +1,89 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Limbo",
+  tagline: "Limbo 是一个分布式社交应用",
+  favicon: "img/favicon.ico",
+
+  url: "https://your-docusaurus-site.example.com",
+  baseUrl: "/",
+
+  organizationName: "sheason",
+  projectName: "Limbo",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  i18n: {
+    defaultLocale: "zh-Hans",
+    locales: ["zh-Hans"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    // image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Limbo",
+      logo: {
+        alt: "Limbo Logo",
+        src: "img/logo.svg",
+        srcDark: "img/logo_dark.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "用户手册",
+        },
+        { to: "/blog", label: "博客", position: "left" },
+        {
+          href: "https://github.com/sheason2019/limbo",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
